806eabc5e78aa2d9b44a7cddbab0c6e771a616ed
Suzana <48587955+StringSuzana@users.noreply.github.com>
2021-06-21 23:17:16 +0200

    sudo

--- project/project_script.sh
+++ project/project_script.sh
@@ -6,20 +6,20 @@ echo "Starting $APP"
  
 cd prod
 pwd
-docker-compose up -d
+sudo docker-compose up -d
 cd ../test
-docker-compose up -d
+sudo docker-compose up -d
 until [ "`docker inspect -f {{.State.Running}} wordpress_prod`"=="true" ]; do
     sleep 0.1;
 done;
 cd ..
-chmod 666 ./prod/wp_data/html/wp-admin/install.php
-chown su:su ./prod/wp_data/html/wp-admin/install.php
+sudo chmod 666 ./prod/wp_data/html/wp-admin/install.php
+sudo chown su:su ./prod/wp_data/html/wp-admin/install.php
 echo 'This is a production site: $APP' > ./prod/wp_data/html/wp-admin/install.php
 until [ "`docker inspect -f {{.State.Running}} wordpress_test`"=="true" ]; do
     sleep 0.1;
 done;
-chmod 666 ./test/wp_data/html/wp-admin/install.php
-chown su:su ./test/wp_data/html/wp-admin/install.php
+sudo chmod 666 ./test/wp_data/html/wp-admin/install.php
+sudo chown su:su ./test/wp_data/html/wp-admin/install.php
 echo 'This is a test site $APP' > ./test/wp_data/html/wp-admin/install.php
 fi
