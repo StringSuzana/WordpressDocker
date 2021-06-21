f9d41362f73294f06e930e8a5f60c5969c213759
Suzana <antolin.suzana@gmail.com>
2021-06-21 22:22:04 +0200

    completed, but CRLF changed!!! 	modified:   project/project_script.sh

--- project/project_script.sh
+++ project/project_script.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-sudo chown su:su ./prod/wp_data/html/wp-admin/install.php
-sudo echo 'This is a production' > ./prod/wp_data/html/wp-admin/install.php
-sudo chown su:su ./test/wp_data/html/wp-admin/install.php
-sudo echo 'This is a test' > ./test/wp_data/html/wp-admin/install.php
+cd prod
+pwd
+docker-compose up -d
+cd ../test
+docker-compose up -d
+until [ "`docker inspect -f {{.State.Running}} wordpress_prod`"=="true" ]; do
+    sleep 0.1;
+done;
+cd ..
+chmod 666 ./prod/wp_data/html/wp-admin/install.php
+chown su:su ./prod/wp_data/html/wp-admin/install.php
+echo 'This is a production site' > ./prod/wp_data/html/wp-admin/install.php
+until [ "`docker inspect -f {{.State.Running}} wordpress_test`"=="true" ]; do
+    sleep 0.1;
+done;
+chmod 666 ./test/wp_data/html/wp-admin/install.php
+chown su:su ./test/wp_data/html/wp-admin/install.php
+echo 'This is a test site' > ./test/wp_data/html/wp-admin/install.php
