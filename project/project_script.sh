ab39d25f3c806f7f62ee157f152ee8e1f9d51853
Suzana <48587955+StringSuzana@users.noreply.github.com>
2021-06-21 23:22:40 +0200

    mkdir

--- project/project_script.sh
+++ project/project_script.sh
@@ -3,23 +3,27 @@ if [ -z ${APP+x} ]; then
   echo "You need to provide app name => APP='some-name'"
 else
 echo "Starting $APP"
- 
+
 cd prod
+sudo mkdir wp_data
+sudo mkdir db_data
 pwd
-sudo docker-compose up -d
+docker-compose up -d
 cd ../test
-sudo docker-compose up -d
+sudo mkdir wp_data
+sudo mkdir db_data
+docker-compose up -d
 until [ "`docker inspect -f {{.State.Running}} wordpress_prod`"=="true" ]; do
     sleep 0.1;
 done;
 cd ..
-sudo chmod 666 ./prod/wp_data/html/wp-admin/install.php
-sudo chown su:su ./prod/wp_data/html/wp-admin/install.php
+chmod 666 ./prod/wp_data/html/wp-admin/install.php
+chown su:su ./prod/wp_data/html/wp-admin/install.php
 echo 'This is a production site: $APP' > ./prod/wp_data/html/wp-admin/install.php
 until [ "`docker inspect -f {{.State.Running}} wordpress_test`"=="true" ]; do
     sleep 0.1;
 done;
-sudo chmod 666 ./test/wp_data/html/wp-admin/install.php
-sudo chown su:su ./test/wp_data/html/wp-admin/install.php
+chmod 666 ./test/wp_data/html/wp-admin/install.php
+chown su:su ./test/wp_data/html/wp-admin/install.php
 echo 'This is a test site $APP' > ./test/wp_data/html/wp-admin/install.php
 fi
