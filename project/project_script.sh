2494b22c6ac8203513ce55644ef56bacd9cd7576
Suzana <48587955+StringSuzana@users.noreply.github.com>
2021-06-21 23:16:35 +0200

    variabla

--- project/project_script.sh
+++ project/project_script.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ -z ${APP+x} ]; then
+  echo "You need to provide app name => APP='some-name'"
+else
+echo "Starting $APP"
+ 
 cd prod
 pwd
 docker-compose up -d
@@ -10,10 +15,11 @@ done;
 cd ..
 chmod 666 ./prod/wp_data/html/wp-admin/install.php
 chown su:su ./prod/wp_data/html/wp-admin/install.php
-echo 'This is a production site' > ./prod/wp_data/html/wp-admin/install.php
+echo 'This is a production site: $APP' > ./prod/wp_data/html/wp-admin/install.php
 until [ "`docker inspect -f {{.State.Running}} wordpress_test`"=="true" ]; do
     sleep 0.1;
 done;
 chmod 666 ./test/wp_data/html/wp-admin/install.php
 chown su:su ./test/wp_data/html/wp-admin/install.php
-echo 'This is a test site' > ./test/wp_data/html/wp-admin/install.php
+echo 'This is a test site $APP' > ./test/wp_data/html/wp-admin/install.php
+fi
