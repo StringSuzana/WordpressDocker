e7e0bbcd230b582c9799229cad983dff3ffa95fe
Suzana <48587955+StringSuzana@users.noreply.github.com>
2021-06-25 15:52:23 +0200

    added sudo

--- project/novaskripta.sh
+++ project/novaskripta.sh
@@ -9,7 +9,7 @@ cd prod
 sudo mkdir wp_data
 sudo mkdir db_data
 pwd
-docker-compose up -d
+sudo docker-compose up -d
 cd ../test
 sudo mkdir wp_data
 sudo mkdir db_data
@@ -26,11 +26,11 @@ do
   echo "."
   sleep 1
 done
-    echo "Writing to production site"
-chmod 666 ./prod/wp_data/html/wp-admin/install.php
-chown root:root ./prod/wp_data/html/wp-admin/install.php
+echo "Writing to production site"
+sudo chmod 666 ./prod/wp_data/html/wp-admin/install.php
+sudo chown root:root ./prod/wp_data/html/wp-admin/install.php
 echo "This is a production site: ${APP}" > ./prod/wp_data/html/wp-admin/install.php
-chown www-data:www-data ./prod/wp_data/html/wp-admin/install.php
+sudo chown www-data:www-data ./prod/wp_data/html/wp-admin/install.php
 until [ "`docker inspect -f {{.State.Running}} wordpress_test`"=="true" ]
 do
     echo "."
@@ -42,8 +42,8 @@ do
   sleep 1
 done
 echo "Writing to test site."
-chmod 666 ./test/wp_data/html/wp-admin/install.php
-chown root:root ./test/wp_data/html/wp-admin/install.php
+sudo chmod 666 ./test/wp_data/html/wp-admin/install.php
+sudo chown root:root ./test/wp_data/html/wp-admin/install.php
 echo "This is a test site ${APP}" > ./test/wp_data/html/wp-admin/install.php
-chown www-data:www-data ./test/wp_data/html/wp-admin/install.php
+sudo chown www-data:www-data ./test/wp_data/html/wp-admin/install.php
 fi
